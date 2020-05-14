54071a1c912e7c569086b0ee03fde3f8b5a37a57
VladaGromova <vladagromova1106@gmail.com>
2020-05-14 12:01:28 +0300

    Favorites created

---
+++ Project33/Project33/Services/Models/Favorites.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Project33.Data;
+namespace Project33.Services.Models
+{
+    public class Favorites
+    {
+        public int id { get; set; }
+        public int book_id { get; set; }
+        public  int user_id { get; set; }
+    }
+}
