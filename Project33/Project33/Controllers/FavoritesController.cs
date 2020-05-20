bb276ea7954f778fac14317b02299612ea7b34cb
VladaGromova <vladagromova1106@gmail.com>
2020-05-20 13:09:02 +0300

    Path to FavorePage

--- Project33/Project33/Controllers/FavoritesController.cs
+++ Project33/Project33/Controllers/FavoritesController.cs
@@ -8,6 +8,9 @@ using Project33.Services.Models;
 using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;//System.Web.Mvc.ActionResult
 using Controller = Microsoft.AspNetCore.Mvc.Controller;
 using Project33.Data;
+using Microsoft.AspNet.Identity;
+using Project33.Services;
+using Project33.Controllers;
 
 namespace Project33.Controllers
 {
@@ -25,6 +28,20 @@ namespace Project33.Controllers
                 book_id = b.book_id,
                 user_id = b.user_id
             }).ToList();
+        }
+        
+        FavoritesContext db_favors = new FavoritesContext();
+        private FavoritesContext _favoritesContext;
+
+        public async Task<IActionResult> Favored()
+        {
+            UserContext user_db = new UserContext();
+            var userName = User.Identity.GetUserName();
+            User user = await user_db.Users.FirstOrDefaultAsync(x => x.Login == userName); // UserId found
+
+            var favors = from f in db_favors.Favorites select f;
+            favors = favors.Where(f => f.user_id.Equals(user.Id));
+            return View(favors.ToList());
         }
     }
 }
